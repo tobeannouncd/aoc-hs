d9c40a2766393a028c248479bd0267d5250cfd3d
tobeannouncd <wzrds3@gmail.com>
2024-12-05 03:19:13 -0500

    update user agent

--- app/Main.hs
+++ app/Main.hs
@@ -26,7 +26,7 @@ getInput year day = do
   session <- getEnv "AOC_SESSION"
   cache <- lookupEnv "AOC_CACHE"
   day' <- maybe (fail $ "invalid day: " ++ show day) return (mkDay day)
-  let agent = AoCUserAgent "tobeannouncd/advent-haskell" "tobeannouncd@gmail.com"
+  let agent = AoCUserAgent "tobeannouncd/aoc-hs" "tobeannouncd@gmail.com"
       opts = (defaultAoCOpts agent year session) {_aCache = cache}
   runAoC_ opts $ AoCInput day'
 
