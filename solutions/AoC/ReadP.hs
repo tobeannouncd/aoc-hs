ddc9fc39477eb847b50302f1b3dd55191d21165b
tobeannouncd <wzrds3@gmail.com>
2024-12-05 19:45:55 -0500

    refactor ReadP to get rid of Read reliance and use more applicative stuff

--- solutions/AoC/ReadP.hs
+++ solutions/AoC/ReadP.hs
@@ -1,12 +1,16 @@
 module AoC.ReadP (
   int,
-  module Text.ParserCombinators.ReadP
+  nat,
+  module Text.ParserCombinators.ReadP,
 ) where
-import Text.ParserCombinators.ReadP
-import Data.Char (isDigit)
 
-int :: (Read a, Num a) => ReadP a
-int = do
-  sign <- option 1 ((-1) <$ char '-')
-  val <- read <$> many1 (satisfy isDigit)
-  return (sign * val)
+import Data.Char (digitToInt, isDigit)
+import Text.ParserCombinators.ReadP
+
+int :: (Integral a) => ReadP a
+int = (*) <$> option 1 (-1 <$ char '-') <*> nat
+
+nat :: (Integral a) => ReadP a
+nat = foldl f 0 <$> many1 (satisfy isDigit)
+ where
+  f a d = 10 * a + fromIntegral (digitToInt d)
