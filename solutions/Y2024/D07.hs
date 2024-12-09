e4ec394e1f5bad61a669193fc587a80a15e7a6aa
tobeannouncd <wzrds3@gmail.com>
2024-12-08 21:24:49 -0500

    add days 7 & 8 to 2024

--- solutions/Y2024/D07.hs
+++ solutions/Y2024/D07.hs
@@ -1,8 +1,27 @@
 module Y2024.D07 (main) where
 
 import Solution
+import AoC.Parsec hiding (getInput)
+import Control.Monad (forM_)
+
+inputP :: Parser [(Integer, [Integer])]
+inputP = sepEndBy1 p newline
+ where
+  p = (,)
+    <$> int <* string ": "
+    <*> sepBy1 int (char ' ')
 
 main :: (Solution m) => m ()
 main = do
-  input <- getInput
-  answerStrLn input
+  equations <- parse' inputP =<< getInput
+  forM_ [[(+),(*)], [(+),(*),concatInt]] $ \ops -> do
+    answer $ sum [t | (t,eqn) <- equations, t `elem` combos ops eqn]
+
+combos :: [a -> a -> a] -> [a] -> [a]
+combos ops (x:xs) = foldl f [x] xs
+ where
+  f acc b = [a `op` b | a <- acc, op <- ops]
+combos _ _ = undefined
+
+concatInt :: Integer -> Integer -> Integer
+concatInt a b = read $ show a ++ show b
--- solutions/Y2024/D08.hs
+++ solutions/Y2024/D08.hs
@@ -1,8 +1,34 @@
 module Y2024.D08 (main) where
 
 import Solution
+import AoC.Coord (from2dString)
+import qualified Data.Map.Strict as Map
+import Data.Ix
+import qualified Data.Set as Set
 
 main :: (Solution m) => m ()
 main = do
-  input <- getInput
-  answerStrLn input
+  input <- from2dString <$> getInput
+  let byFreq = Map.fromListWith (++) [(v,[k]) | (k,v) <- input, v /= '.']
+      inGrid = inRange (fst $ head input, fst $ last input)
+  answer $ solveWith (part1 inGrid) byFreq
+  answer $ solveWith (part2 inGrid) byFreq
+
+solveWith :: Ord a1 => (a2 -> [a1]) -> Map.Map k a2 -> Int
+solveWith f = Set.size . Set.unions . map (Set.fromList . f) . Map.elems
+
+part1,part2 :: Num a => (a -> Bool) -> [a] -> [a]
+part1 inGrid pts =
+  [ pt
+  | (a,b) <- pairs pts
+  , pt <- [2*a-b, 2*b-a]
+  , inGrid pt ]
+part2 inGrid pts =
+  [ pt
+  | (a,b) <- pairs pts
+  , (start, dir) <- [(a,a-b),(b,b-a)]
+  , pt <- takeWhile inGrid $ iterate (+ dir) start ]
+
+pairs :: [a] -> [(a,a)]
+pairs [] = []
+pairs (x:xs) = [(x,y) | y <- xs] ++ pairs xs
