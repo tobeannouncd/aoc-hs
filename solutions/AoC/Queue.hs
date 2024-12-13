a0903e0c4cdcb58a715086b09a6e8bae8d3137aa
tobeannouncd <wzrds3@gmail.com>
2024-12-12 19:45:10 -0500

    re-implement FIFO queue

--- solutions/AoC/Queue.hs
+++ solutions/AoC/Queue.hs
@@ -1,15 +1,24 @@
-{-|
-https://github.com/glguy/advent/blob/main/common/src/Advent/Queue.hs
--}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
 module AoC.Queue (
-  Queue((:<|),Empty), (|>),
-  singleton, fromList, snoc, pop, appendList
+  Queue((:<|),Empty),
+  (|>), snoc, pop,
+  singleton, fromList, appendList
 ) where
+
 import Data.Foldable (Foldable(..))
 
+-- | @Queue front rear difference@
+--
+--   The following invariants hold:
+--
+-- > length front >= length rear
+-- > difference == length front - length rear
 data Queue a = Queue [a] [a] !Int
+  deriving Functor
+
+instance Show a => Show (Queue a) where
+  show q = "fromList " ++ show (toList q)
 
 {-# COMPLETE (:<|), Empty #-}
 
@@ -19,24 +28,25 @@ pattern Empty <- Queue [] _ _
   Empty = Queue [] [] 0
 
 pattern (:<|) :: a -> Queue a -> Queue a
-pattern x :<| xs <- (pop -> Just (x, xs))
+pattern x :<| xs <- (pop -> Just (x,xs))
 
 (|>) :: Queue a -> a -> Queue a
-q |> x = snoc x q
+(|>) = flip snoc
 {-# INLINE (|>) #-}
 
 instance Foldable Queue where
-  null (Queue l _ _) = null l
-  length (Queue l r _) = length l + length r
-  elem x (Queue l r _) = elem x l || elem x r
-  sum (Queue l r _) = sum l + sum r
-  foldMap _ (Queue [] _ _) = mempty
-  foldMap f (Queue (x:l) r 0) = f x <> rot l r
+  foldMap _ (Queue []    _ _) = mempty
+  foldMap f (Queue (x:l) r 0) = f x <> go l r
    where
-    rot [] (y:_) = f y
-    rot (x':xs) (y:ys) = f x' <> rot xs ys <> f y
-    rot _ _ = error "Queue invariant violated"
-  foldMap f (Queue (x:l) r i) = f x <> foldMap f (Queue l r (i-1))
+    go []      (y: _) = f y
+    go (x':l') (y:ys) = f x' <> go l' ys <> f y
+    go _       _      = error "Queue invariant violated"
+  foldMap f (Queue (x:l) r d) = f x <> foldMap f (Queue l r (d-1))
+  null    (Queue l _ _) = null l
+  length  (Queue _ r d) = 2*length r + d
+  elem x  (Queue l r _) = elem x l || elem x r
+  sum     (Queue l r _) = sum l + sum r
+  product (Queue l r _) = product l * product r
 
 singleton :: a -> Queue a
 singleton x = Queue [x] [] 1
@@ -47,20 +57,21 @@ fromList xs = Queue xs [] (length xs)
 appendList :: Queue a -> [a] -> Queue a
 appendList = foldl' (|>)
 
-
 snoc :: a -> Queue a -> Queue a
-snoc x (Queue f r s) = exec f (x:r) s
+snoc x (Queue f r d) = rebalance f (x:r) d
 
-exec :: [a] -> [a] -> Int -> Queue a
-exec f r 0 = fromList (rotate f r [])
-exec f r i = Queue f r (i-1)
+rebalance :: [a] -> [a] -> Int -> Queue a
+rebalance f r 0 = fromList (rotate f r [])
+rebalance f r i = Queue f r (i-1)
 
+-- | Lazily converts the front and rear of a queue into
+--   a single list.
 rotate :: [a] -> [a] -> [a] -> [a]
-rotate [] (y:_) a = y : a
-rotate (x:xs) (y:ys) a = x : rotate xs ys (y:a)
+rotate [] (y:_) acc = y:acc
+rotate (x:xs) (y:ys) acc = x : rotate xs ys (y:acc)
 rotate _ _ _ = error "Queue invariant violated"
 
 
 pop :: Queue a -> Maybe (a, Queue a)
-pop (Queue (x:f) r s) = Just (x, exec f r s)
+pop (Queue (x:f) r d) = Just (x, rebalance f r d)
 pop _ = Nothing
