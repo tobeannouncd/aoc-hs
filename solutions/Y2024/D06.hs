04f55985163cc7a41141ab304879a4a0e0328565
tobeannouncd <wzrds3@gmail.com>
2024-12-12 21:20:54 -0500

    optimize days 6 & 9 of 2024

--- solutions/Y2024/D06.hs
+++ solutions/Y2024/D06.hs
@@ -2,53 +2,35 @@ module Y2024.D06 (main) where
 
 import AoC.Coord
 import Data.Containers.ListUtils (nubOrd)
-import Data.List (unfoldr)
 import Data.Map (Map, (!?))
 import Data.Map.Strict qualified as Map
-import Data.Set qualified as S
 import Solution
+import AoC (countIf)
 
 main :: (Solution m) => m ()
 main = do
   (guardPos, facing, tileMap) <- process . from2dString <$> getInput
-  let guardPath = nubOrd . map fst $ unfoldr (walk tileMap) start
+  let guardPath = nubOrd . map fst $ walk' tileMap start
       start = (guardPos, facing)
+      check pt = isLoop $ walk' (Map.insert pt Obstacle tileMap) start
   answer $ length guardPath
-  -- | I know this isn't efficient. I'll optimize later.
-  answer $
-    length
-      [ ()
-      | pt <- guardPath
-      , let tileMap' = Map.insert pt Obstacle tileMap
-      , isLoop $ unfoldr (walk tileMap') start
-      ]
+  answer $ countIf check (tail guardPath)
 
-isLoop :: [(Coord, Coord)] -> Bool
-isLoop = go S.empty
+walk' :: Map Coord Tile -> (Coord, Coord) -> [(Coord, Coord)]
+walk' grid = uncurry go
  where
-  go _ [] = False
-  go seen (here : rest) =
-    here `S.member` seen
-      || go (S.insert here seen) rest
+  go pos dir = (pos, dir) :
+    case grid !? (pos + dir) of
+      Nothing   -> []
+      Just Open -> go (pos + dir) dir
+      _         -> go pos (clockwise dir)
 
-walk :: Map Coord Tile -> (Coord, Coord) -> Maybe ((Coord, Coord), (Coord, Coord))
-walk tileMap = go
+-- | Tortoise & hare loop detection
+isLoop :: Eq a => [a] -> Bool
+isLoop lst = go lst lst
  where
-  go (here, facing) =
-    if here `Map.notMember` tileMap
-      then Nothing
-      else Just ((here, facing), next)
-   where
-    next = f surround
-    surround =
-      [ (here + x, x)
-      | x <- [facing, clockwise facing, -facing, counterclockwise facing]
-      ]
-    f (x@(there, _) : nxt) =
-      case tileMap !? there of
-        Just Obstacle -> f nxt
-        _ -> x
-    f _ = undefined
+  go (x:xs) (_:y:ys) = x == y || go xs ys
+  go _ _ = False
 
 data Tile
   = Obstacle
@@ -65,7 +47,7 @@ process xs = (guardPos, facing, tileMap)
       '<' -> west
       '^' -> north
       'v' -> south
-      _ -> error "no match"
+      _   -> error "no match"
   tileMap =
     Map.fromList
       [ (c, if v == '#' then Obstacle else Open)
--- solutions/Y2024/D09.hs
+++ solutions/Y2024/D09.hs
@@ -2,44 +2,73 @@ module Y2024.D09 (main) where
 
 import Solution
 import Data.Char
-import AoC (countIf)
-import Data.Maybe (isJust, isNothing)
-import Data.List (group)
-import Control.Monad (void)
+import Data.Maybe (listToMaybe)
+import Data.Array.Unboxed (UArray, bounds, (!), accumArray)
+import qualified Data.IntMap as IntMap
 
 main :: (Solution m) => m ()
 main = do
   input <- map digitToInt . head . lines <$> getInput
-  let disk = unpack input
-  answer $ checksum $ part1 disk
-  answer $ checksum $ part2 disk
+  let (files, free) = decode input
+  answer $ part1 (expand (sum input) files)
+  answer $ part2 files free
 
-checksum :: [Maybe Int] -> Int
-checksum = sum . zipWith (\i -> maybe 0 (i*)) [0..]
 
-unpack :: [Int] -> [Maybe Int]
-unpack = go 0
+-- | converts the input list into a representation of the files and free space
+--   in the form of @([(start, fileId, size)], [(start, size)])@
+--
+--   Both lists are in reversed order from the disk order.
+decode :: [Int] -> ([(Int,Int,Int)], [(Int,Int)])
+decode = goFile [] [] 0 0
  where
-  go _ [] = []
-  go i [x] = replicate x (Just i)
-  go i (x:y:ys) = replicate x (Just i) ++ replicate y Nothing ++ go (succ i) ys
+  goFile files free i pos = \case
+    []   -> (files, free)
+    x:xs -> goFree ((pos,i,x):files) free (succ i) (pos + x) xs
+  goFree files free i pos = \case
+    []   -> (files, free)
+    0:xs -> goFile files free i pos xs
+    x:xs -> goFile files ((pos,x):free) i (pos+x) xs
 
-part1,part2 :: [Maybe Int] -> [Maybe Int]
-part1 xs = take len $ go xs $ reverse (filter isJust xs)
+expand :: Int -> [(Int,Int,Int)] -> UArray Int Int
+expand end files =
+  accumArray (\_ x -> x) (-1) (0,end)
+    [ (pos, fileID)
+    | (pos0, fileID, size) <- files
+    , pos <- [pos0 .. pos0 + size - 1]]
+
+-- | Works from both ends of the disk to calculate the checksum on-the-fly
+part1 :: UArray Int Int -> Int
+part1 disk = sum $ uncurry go (bounds disk)
  where
-  len = countIf isJust xs
-  go ys [] = ys
-  go (Nothing:ys) (z:zs) = z:go ys zs
-  go (y:ys) zs = y:go ys zs
-  go [] _ = []
-part2 disk
-  | Just n <- last disk = concat $ foldr move (group disk) [0..n]
-  | otherwise = undefined
-  where
-    move i grps =
-      case break ((== [Just i]) . take 1) grps of
-        (xs, g:ys) -> let lenG = length g in
-          case break (\x -> void x >= void g && isNothing (head x)) xs of
-            (xs', x:xs'') -> xs' ++ g : drop lenG x : xs'' ++ replicate lenG Nothing : ys
-            _ -> grps
-        _ -> undefined
+  go i j
+    | i > j     = []
+    | l >= 0    = (i * l) : go (i+1) j
+    | r >= 0    = (i * r) : go (i+1) (j-1)
+    | otherwise = go i (j-1)
+   where
+    l = disk ! i
+    r = disk ! j
+
+part2 :: [(Int,Int,Int)] -> [(Int,Int)] -> Int
+part2 files freeSpace = fst $ foldl move (0, IntMap.fromList freeSpace) files
+ where
+  move (acc, free) (pos, fileId, fileSize) =
+    let free1 = IntMap.takeWhileAntitone (< pos) free in
+      case findFree fileSize free1 of
+        Nothing            -> (acc + checksum' pos  fileId fileSize, free1)
+        Just (pos', free2) -> (acc + checksum' pos' fileId fileSize, free2)
+
+  findFree fileSize free = listToMaybe
+    [ (pos, free2)
+    | (pos, freeSize) <- IntMap.toAscList free
+    , freeSize >= fileSize
+    , let free1 = IntMap.delete pos free
+          free2 =
+            if freeSize == fileSize
+              then free1
+              else IntMap.insert (pos + fileSize) (freeSize - fileSize) free1
+    ]
+
+  checksum' pos fileId fileSize =
+    fileId * (2 * pos + fileSize - 1) * fileSize `div` 2
+
