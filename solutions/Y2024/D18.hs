257f959f303e10766b9cee15a491776630f1baa4
tobeannouncd <wzrds3@gmail.com>
2024-12-18 00:49:30 -0500

    refactor 2024-18

--- solutions/Y2024/D18.hs
+++ solutions/Y2024/D18.hs
@@ -6,6 +6,7 @@ import AoC.Coord
 import Data.Ix
 import qualified Data.Set as S
 import AoC.Search
+import qualified Data.Map as M
 
 start, end :: Coord
 start = 0
@@ -17,13 +18,23 @@ inputP = flip C <$> nat <* char ',' <*> nat
 main :: Solution m => m ()
 main = do
   input <- parse' (sepEndBy1 inputP newline) =<< getInput
-  let next pt = let corrupt = S.fromList (take 1024 input) in
+  let corrupt = M.fromList $ zip input [0 :: Int ..]
+      next pt =
         [ AStep pt' 1 (manhattan pt' end)
-        | pt' <- cardinal pt, inRange (start,end) pt', pt' `S.notMember` corrupt ]
+        | pt' <- cardinal pt, inRange (start,end) pt'
+        , case corrupt M.!? pt' of
+            Just i -> i >= 1024
+            _      -> True
+        ]
   answer $ head [cost | (pt,cost) <- astar next start, pt == end]
 
-  let next' n pt = let corrupt = S.fromList (take n input) in
-        [pt' | pt' <- cardinal pt, inRange (start,end) pt', pt' `S.notMember` corrupt]
-      nStop = head [n | n <- [1025..], end `notElem` dfs (next' n) start] - 1
+  let next' n pt =
+        [ pt'
+        | pt' <- cardinal pt, inRange (start,end) pt'
+        , case corrupt M.!? pt' of
+            Just i -> i > n
+            _      -> True
+        ]
+      nStop = head [n | n <- [1024..], end `notElem` dfs (next' n) start]
       C yS xS = input !! nStop
   answerStrLn $ show xS ++ "," ++ show yS
